55f88ea5e85c4ae6b899aede261dec212c08102b
Kirtan <kirtanpatel@billericak12.com>
2021-09-10 11:34:37 -0400

    djidnjvfnjksdkl

--- src/com/company/Yummyfood.java
+++ src/com/company/Yummyfood.java
@@ -1,4 +1,22 @@
 package com.company;
 
 public class Yummyfood {
+    public static void main(String[] args) {
+        System.out.print("yummy");
+        System.out.println("food");
+        chocolate();
+        chocolate();
+        icecream();
+        System.out.println("done");
+    }
+
+    public static void chocolate(){
+        System.out.println("chocolate");
+    }
+
+    public static void icecream() {
+        System.out.println("chunky chocolate pudding");
+        chocolate();
+        System.out.println("that was yummy");
+    }
 }
